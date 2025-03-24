560f3ac5a299d449a8a6b862b7aa7b2d8ba3141c
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2025-03-24 19:11:04 -0300

    txt

--- config/database.php
+++ config/database.php
@@ -82,21 +82,22 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'require',   
-           'options' => extension_loaded('pgsql') ? ['options' => env('DB_OPTIONS')] : [],
-        ],
+       'pgsql' => [
+        'driver' => 'pgsql',
+        'url' => env('DB_URL'),
+        'host' => env('DB_HOST', '127.0.0.1'),
+        'port' => env('DB_PORT', '5432'),
+        'database' => env('DB_DATABASE', 'laravel'),
+        'username' => env('DB_USERNAME', 'root'),
+        'password' => env('DB_PASSWORD', ''),
+        'charset' => env('DB_CHARSET', 'utf8'),
+        'prefix' => '',
+        'prefix_indexes' => true,
+        'search_path' => 'public',
+        'sslmode' => 'require',   
+        'options' => env('DB_OPTIONS') ? [env('DB_OPTIONS')] : [],
+    ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
