6bb55e0802edbd39d2173ec54af80876fe24e092
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2025-03-25 12:25:26 -0300

    laravel deploy

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -81,23 +81,6 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
-       'pgsql' => [
-        'driver' => 'pgsql',
-        'url' => env('DB_URL'),
-        'host' => env('DB_HOST', '127.0.0.1'),
-        'port' => env('DB_PORT', '5432'),
-        'database' => env('DB_DATABASE', 'laravel'),
-        'username' => env('DB_USERNAME', 'root'),
-        'password' => env('DB_PASSWORD', ''),
-        'charset' => env('DB_CHARSET', 'utf8'),
-        'prefix' => '',
-        'prefix_indexes' => true,
-        'search_path' => 'public',
-        'sslmode' => 'require',   
-        'options' => env('DB_OPTIONS') ? [env('DB_OPTIONS')] : [],
-    ],
-
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
